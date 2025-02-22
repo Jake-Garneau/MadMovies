cf71dfb2434f762c1fc3afeed6003734569a664f
Colin Shaw <cjshaw3@wisc.edu>
2025-02-22 14:48:27 -0600

    App.js changes and pages added

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import LanguageSelect from './pages/LanguageSelect';
+import GenreSelect from './pages/GenreSelect';
+import RuntimeSelect from './pages/RuntimeSelect';
+import ReleaseDateSelect from './pages/ReleaseDateSelect';
+import BudgetSelect from './pages/BudgetSelect';
+import Result from './pages/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/language" element={<LanguageSelect />} />
+        <Route path="/genre" element={<GenreSelect />} />
+        <Route path="/era" element={<ReleaseDateSelect />} />
+        <Route path="/runtime" element={<RuntimeSelect />} />
+        <Route path="/budget" element={<BudgetSelect />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
