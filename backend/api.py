a41383d7a3703afbabdf5ed33b70adf78f685720
Jake Garneau <jgarneau@wisc.edu>
2025-02-22 12:31:50 -0600

    starting db connection

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+import pandas as pd
+import sqlite3
 
 app = FastAPI()
+
+name = 'IMDBmovies.db'
+con = sqlite3.connect(name)
+
+def get_movies_from_db():
+    query="select * from sqlite_master"
+    df = pd.read_sql(query, con)
+    return df.to_dict(orient="records")
 
 @app.get("/")
 def read_root():
